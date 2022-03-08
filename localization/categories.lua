51f03c2d834042683dd1250205e0c42cc767dc7d
Macumba <85767653+Macumbatbc@users.noreply.github.com>
2022-03-08 14:06:31 +0100

    Add files via upload

---
+++ localization/categories.lua
@@ -0,0 +1,18 @@
+local L = AfflictedLocals
+L.spells = {
+	["feral"] = L["Feral Charge"],
+	["fire"] = L["Fire traps"],
+	["frost"] = L["Frost traps"],
+	["nature"] = L["Nature traps"],
+}
+L.classes = {
+	["DRUID"] = L["Druid"],
+	["HUNTER"] = L["Hunter"],
+	["MAGE"] = L["Mage"],
+	["PALADIN"] = L["Paladin"],
+	["PRIEST"] = L["Priest"],
+	["ROGUE"] = L["Rogue"],
+	["SHAMAN"] = L["Shaman"],
+	["WARLOCK"] = L["Warlock"],
+	["WARRIOR"] = L["Warrior"],
+}
